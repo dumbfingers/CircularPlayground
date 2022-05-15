efe8d32c4048b2087bda2bb92c38c48dcf8b5a4c
Yaxi Ye <yaxi@whereismytransport.com>
2022-05-15 12:00:51 +0100

    add network module in DI

---
+++ app/src/main/java/com/example/circularplayground/di/NetworkModule.kt
@@ -0,0 +1,38 @@
+package com.example.circularplayground.di
+
+import com.example.circularplayground.data.network.CircularService
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import okhttp3.OkHttpClient
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
+import javax.inject.Singleton
+
+@InstallIn(SingletonComponent::class)
+@Module
+class NetworkModule {
+
+    @Singleton
+    @Provides
+    fun provideOkHttpClient(): OkHttpClient {
+        return OkHttpClient.Builder()
+            .readTimeout(30, TimeUnit.SECONDS)
+            .connectTimeout(30, TimeUnit.SECONDS)
+            .build()
+    }
+
+    @Singleton
+    @Provides
+    fun provideCircularService(client: OkHttpClient): CircularService {
+        val baseUrl = " https://android-interview.s3.eu-west-2.amazonaws.com/"
+        return Retrofit.Builder()
+            .baseUrl(baseUrl)
+            .client(client)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+            .create(CircularService::class.java)
+    }
+}
