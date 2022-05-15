f67b65d8898deb3003c81a9941eee09143369191
Yaxi Ye <yaxi@whereismytransport.com>
2022-05-15 18:19:23 +0100

    add circular indicator to main screen, add view state for main screen

---
+++ app/src/main/java/com/example/circularplayground/MainViewState.kt
@@ -0,0 +1,6 @@
+package com.example.circularplayground
+
+data class MainViewState(
+    val currentScore: Int = 0,
+    val totalScore: Int = 0
+)
--- app/src/main/java/com/example/circularplayground/ui/MainScreen.kt
+++ app/src/main/java/com/example/circularplayground/ui/MainScreen.kt
@@ -1,31 +1,45 @@
 package com.example.circularplayground.ui
 
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.circularplayground.MainViewState
 import com.example.circularplayground.ui.theme.CircularPlaygroundTheme
 
 @Composable
-internal fun MainScreen() {
-    Surface(modifier = Modifier.fillMaxSize(),
-        color = MaterialTheme.colors.background) {
-        Greeting("Android")
+internal fun MainScreen(
+    viewState: MainViewState,
+) {
+    Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colors.secondary) {
+        Box(
+            modifier = Modifier.fillMaxSize(),
+            contentAlignment = Alignment.Center
+        ) {
+            ScoreCircularIndicator(
+                currentScore = viewState.currentScore,
+                totalScore = viewState.totalScore,
+                onClick = {}
+            )
+        }
     }
-}
-
-@Composable
-internal fun Greeting(name: String) {
-    Text(text = "Hello $name!")
 }
 
 @Preview(showBackground = true)
 @Composable
 private fun DefaultPreview() {
     CircularPlaygroundTheme {
-        MainScreen()
+        MainScreen(
+            MainViewState(327, 700)
+        )
     }
 }
