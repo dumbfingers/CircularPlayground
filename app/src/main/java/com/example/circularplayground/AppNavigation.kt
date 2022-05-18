b4e0a888b65a9e37b888bf8479a863c9a8af313f
Yaxi Ye <yaxi@whereismytransport.com>
2022-05-18 12:18:59 +0100

    add navigation controller

---
+++ app/src/main/java/com/example/circularplayground/AppNavigation.kt
@@ -0,0 +1,46 @@
+package com.example.circularplayground
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavHostController
+import com.example.circularplayground.ui.DetailScreen
+import com.google.accompanist.navigation.animation.composable
+import com.example.circularplayground.ui.MainScreen
+import com.google.accompanist.navigation.animation.AnimatedNavHost
+
+sealed class Screens(private val route: String) {
+    fun createRoute() = this.route
+    object MainScreen: Screens(route = "main")
+    object DetailScreen: Screens(route = "detail?ref={clientRef}") {
+        fun createRoute(clientRef: String) = "detail?ref=$clientRef"
+    }
+}
+
+@Composable
+fun AppNavigation(
+    modifier: Modifier = Modifier,
+    navController: NavHostController,
+) {
+    AnimatedNavHost(
+        modifier = modifier,
+        navController = navController,
+        startDestination = Screens.MainScreen.createRoute(),
+    ) {
+
+        composable(
+            route = Screens.MainScreen.createRoute()
+        ) {
+            MainScreen(
+                openDetails = {
+                    navController.navigate(Screens.DetailScreen.createRoute(clientRef = it))
+                },
+            )
+        }
+
+        composable(
+            route = Screens.DetailScreen.createRoute()
+        ) {
+            DetailScreen()
+        }
+    }
+}
--- app/src/main/java/com/example/circularplayground/MainActivity.kt
+++ app/src/main/java/com/example/circularplayground/MainActivity.kt
@@ -3,23 +3,22 @@ package com.example.circularplayground
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.material.Scaffold
 import com.example.circularplayground.ui.theme.CircularPlaygroundTheme
+import com.google.accompanist.navigation.animation.rememberAnimatedNavController
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity: ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
+            val navHostController = rememberAnimatedNavController()
             CircularPlaygroundTheme {
-                // A surface container using the 'background' color from the theme
-
+                Scaffold {
+                    AppNavigation(navController = navHostController)
+                }
             }
         }
     }
