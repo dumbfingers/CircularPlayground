f56d044ddfc45cff0a56e9e431dad23969096e40
Yaxi Ye <yaxi@whereismytransport.com>
2022-05-18 12:15:08 +0100

    add App class, add dispatcher data class for DI

--- app/src/main/java/com/example/circularplayground/di/AppModule.kt
+++ app/src/main/java/com/example/circularplayground/di/AppModule.kt
@@ -1,11 +1,18 @@
 package com.example.circularplayground.di
 
 import android.content.Context
+import androidx.datastore.core.DataStore
+import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.preferencesDataStore
+import androidx.datastore.preferences.preferencesDataStoreFile
+import com.example.circularplayground.utils.CoroutineDispatchers
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
+import kotlinx.coroutines.Dispatchers
+import javax.inject.Singleton
 
 @InstallIn(SingletonComponent::class)
 @Module
@@ -16,4 +23,12 @@ class AppModule {
 
     @Provides
     fun provideContext(@ApplicationContext context: Context) = context
+
+    @Singleton
+    @Provides
+    fun provideCoroutineDispatchers() = CoroutineDispatchers(
+        io = Dispatchers.IO,
+        default = Dispatchers.Default,
+        main = Dispatchers.Main
+    )
 }
---
+++ app/src/main/java/com/example/circularplayground/utils/CoroutineDispatchers.kt
@@ -0,0 +1,9 @@
+package com.example.circularplayground.utils
+
+import kotlinx.coroutines.CoroutineDispatcher
+
+data class CoroutineDispatchers(
+    val io: CoroutineDispatcher,
+    val default: CoroutineDispatcher,
+    val main: CoroutineDispatcher
+)
