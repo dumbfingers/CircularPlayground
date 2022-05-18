1f70a4335e525d0d83a226f8faa2af5571144758
Yaxi Ye <yaxi@whereismytransport.com>
2022-05-18 12:19:25 +0100

    tweak for circular indicator

--- app/src/main/java/com/example/circularplayground/ui/ScoreCircularIndicator.kt
+++ app/src/main/java/com/example/circularplayground/ui/ScoreCircularIndicator.kt
@@ -4,7 +4,6 @@ import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
@@ -21,12 +20,9 @@ import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.semantics.Role
 import androidx.compose.ui.text.AnnotatedString
 import androidx.compose.ui.text.SpanStyle
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
-import androidx.constraintlayout.compose.ConstraintLayout
 import com.example.circularplayground.R
 import com.example.circularplayground.ui.theme.CircularPlaygroundTheme
 
@@ -35,10 +31,12 @@ internal fun ScoreCircularIndicator(
     modifier: Modifier = Modifier,
     currentScore: Int,
     totalScore: Int,
-    onClick: () -> Unit
+    isLoading: Boolean,
+    onClick: () -> Unit,
 ) {
     Box(
-        modifier = modifier.size(192.dp)
+        modifier = modifier
+            .size(192.dp)
             .clickable(
                 role = Role.Button,
                 onClick = onClick
@@ -55,24 +53,28 @@ internal fun ScoreCircularIndicator(
                 .clip(CircleShape),
             contentAlignment = Alignment.Center
         ) {
-            val progressString = currentScore.toString()
-            val titleString = stringResource(id = R.string.title_credit_score, progressString)
-            val index = titleString.indexOf(progressString)
-            val style = MaterialTheme.typography.h2
-            val annotatedString = AnnotatedString(
-                text = titleString,
-                spanStyles = listOf(
-                    AnnotatedString.Range(
-                        item = SpanStyle(
-                            fontSize = style.fontSize,
-                            fontWeight = style.fontWeight,
-                            color = MaterialTheme.colors.primary
-                        ),
-                        start = index,
-                        end = index + progressString.length
+            val annotatedString = if (isLoading) {
+                AnnotatedString(stringResource(id = R.string.title_loading))
+            } else {
+                val progressString = currentScore.toString()
+                val titleString = stringResource(id = R.string.title_credit_score, progressString)
+                val index = titleString.indexOf(progressString)
+                val style = MaterialTheme.typography.h2
+                AnnotatedString(
+                    text = titleString,
+                    spanStyles = listOf(
+                        AnnotatedString.Range(
+                            item = SpanStyle(
+                                fontSize = style.fontSize,
+                                fontWeight = style.fontWeight,
+                                color = MaterialTheme.colors.primary
+                            ),
+                            start = index,
+                            end = index + progressString.length
+                        )
                     )
                 )
-            )
+            }
             Text(
                 text = annotatedString,
                 style = MaterialTheme.typography.caption,
@@ -91,18 +93,33 @@ internal fun ScoreCircularIndicator(
                 )
             },
         )
-        CircularProgressIndicator(
-            modifier = Modifier.fillMaxSize(0.9f),
-            progress = currentScore.toFloat() / totalScore.toFloat(),
-            strokeWidth = 2.dp,
-        )
+        if (isLoading) {
+            CircularProgressIndicator(
+                modifier = Modifier.fillMaxSize(0.9f),
+                strokeWidth = 2.dp,
+            )
+        } else {
+            CircularProgressIndicator(
+                modifier = Modifier.fillMaxSize(0.9f),
+                progress = currentScore.toFloat() / totalScore.toFloat(),
+                strokeWidth = 2.dp,
+            )
+        }
     }
 }
 
 @Preview
 @Composable
-private fun ScoreCircularIndicator() {
+private fun ScoreCircularIndicatorLoaded() {
     CircularPlaygroundTheme {
-        ScoreCircularIndicator(currentScore = 327, totalScore = 700) {}
+        ScoreCircularIndicator(isLoading = false, currentScore = 327, totalScore = 700) {}
+    }
+}
+
+@Preview
+@Composable
+private fun ScoreCircularIndicatorLoading() {
+    CircularPlaygroundTheme {
+        ScoreCircularIndicator(isLoading = true, currentScore = 327, totalScore = 700) {}
     }
 }
